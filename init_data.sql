1119344abcd8c7f23b344ae5305cbbdcadb2f0f2
Michal Gugala <michal.gugala@icloud.com>
2023-08-15 17:55:08 +0200

    add docker compose with postgres, add sql file which inits the database. remove redundant files.

---
+++ init_data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    UNIQUE(username, user_id)
+)
