a5710fada522651d703115e8a4697d9dcd98ad65
Michal Gugala <michal.gugala@icloud.com>
2023-08-15 18:21:41 +0200

    fix type mismatch between postgres and axum

--- init_data.sql
+++ init_data.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     email VARCHAR(255) NOT NULL,
     UNIQUE(username, user_id)
-)
+);
+
+insert into users (username, created_at, email) values ('foo.b', current_timestamp, 'foo.bar@icloud.com');
