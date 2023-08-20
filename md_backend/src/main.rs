02a0d644297812706b7d22ad816211f2e1bc7125
Michal Gugala <michal.gugala@icloud.com>
2023-08-21 00:31:50 +0200

    add post for users

--- md_backend/src/main.rs
+++ md_backend/src/main.rs
@@ -1,8 +1,8 @@
-use axum::{extract::State, http::StatusCode, routing::get, Json, Router};
+use axum::{debug_handler, extract::State, http::StatusCode, routing::get, Json, Router};
 use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
 use tokio_postgres::NoTls;
-use users::User;
+use users::{CreateUser, User};
 pub mod users;
 #[tokio::main]
 async fn main() {
@@ -15,7 +15,7 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(|| async { "Hello World!" }))
-        .route("/api/users", get(get_users))
+        .route("/api/users", get(get_users).post(post_users))
         .with_state(pool);
 
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
@@ -48,8 +48,34 @@ async fn get_users(
 
     Ok(Json(users))
 }
-
-async fn post_users() {}
+async fn post_users(
+    State(pool): State<ConnectionPool>,
+    Json(input): Json<CreateUser>,
+) -> Result<Json<User>, (StatusCode, String)> {
+    let new_user = CreateUser {
+        username: input.username,
+        email: input.email,
+    };
+    let connection = pool.get().await.map_err(|e| internal_error(e))?;
+    let add_user_query = connection
+        .query(
+            "INSERT into users (username, created_at, email) values ($1, $2, $3) RETURNING *;",
+            &[
+                &new_user.username,
+                &time::OffsetDateTime::now_local().map_err(|e| internal_error(e))?,
+                &new_user.email,
+            ],
+        )
+        .await
+        .map_err(|e| internal_error(e))?;
+    let added_user_row = &add_user_query[0];
+    Ok(Json(User::new(
+        added_user_row.get("user_id"),
+        added_user_row.get("username"),
+        added_user_row.get("created_at"),
+        added_user_row.get("email"),
+    )))
+}
 
 fn internal_error<E>(err: E) -> (StatusCode, String)
 where
